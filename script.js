277125309fae27dee3970479110954c65ebb4ec4
Marcin <marcinstarzomski@gmail.com>
2022-03-20 13:26:38 +0100

    Add run five rounds of the game

--- script.js
+++ script.js
@@ -4,7 +4,7 @@ function computerPlay() {
     let weapon = weapons[Math.floor(Math.random()*weapons.length)];
     return weapon;
 }
-
+//Logic of RPS
 function playRound(playerSelection, computerSelection) {
     if (playerSelection === computerSelection) {
         return "It's a draw";
@@ -14,24 +14,33 @@ function playRound(playerSelection, computerSelection) {
         } else {
             return "You won!";
         }
-    } //else if (playerSelection === "paper") {
-    //     if (computerSelection === "scissors") {
-    //         return "Computer wins!";
-    //     } else {
-    //         return "You won!";
-    //     }
-    // } else if (playerSelection === "scissors") {
-    //     if (computerSelection === "rock") {
-    //         return "Computer wins!";
-    //     } else {
-    //         return "You won!";
-    //     }
-    // }
+    } else if (playerSelection === "paper") {
+        if (computerSelection === "scissors") {
+            return "Computer wins!";
+        } else {
+            return "You won!";
+        }
+    } else if (playerSelection === "scissors") {
+        if (computerSelection === "rock") {
+            return "Computer wins!";
+        } else {
+            return "You won!";
+        }
+    }
+  }
+
+//Five rounds of the game
+function game() {
+    for (let i = 0; i < 5; i++) {
+        let computerSelection = computerPlay();
+        playRound(playerSelection, computerSelection);
+        console.log("You choosed " + playerSelection);
+        console.log("Computer choosed " + computerSelection);
+        console.log(playRound(playerSelection, computerSelection));
+     }
   }
 
 const playerSelection= "rock".toLowerCase();
 const computerSelection = computerPlay();
 
-console.log("You choosed " + playerSelection);
-console.log("Computer choosed " + computerSelection);
-console.log(playRound(playerSelection, computerSelection));
+game();
